8afd053286dabdb77d62df6ac96f69dbc0b65196
adidtya <pvtyanz@gmail.com>
2024-02-28 11:44:26 +0700

    menambahkan one to one

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+/**
+ * @path /users
+ * @method GET
+ */
+Route::get('/users', [\App\Http\Controllers\UserController::class, 'index']);
+
+/**
+ * @path /posts
+ * @method GET
+ */
+Route::get('/posts', [\App\Http\Controllers\PostController::class, 'index']);
